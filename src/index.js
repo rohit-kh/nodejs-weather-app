56d378ff4e75c326d9e67a961e72d69740025546
rohit_kumar_vfirst <rohit_kumar_vfirst@bitbucket.org>
2021-04-12 19:25:56 +0530

    Change name

--- src/index.js
+++ src/index.js
@@ -82,7 +82,7 @@ app.get('/products', (req, res) => {
 app.get('/help/*', (req, res) => {
     res.render('404', {
         title: '404',
-        name: 'Andrew Mead',
+        name: 'Rohit Khatri',
         errorMessage: 'Help article not found.'
     })
 })
@@ -90,7 +90,7 @@ app.get('/help/*', (req, res) => {
 app.get('*', (req, res) => {
     res.render('404', {
         title: '404',
-        name: 'Andrew Mead',
+        name: 'Rohit Khatri',
         errorMessage: 'Page not found.'
     })
 })
